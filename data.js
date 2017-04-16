048ca545d6c6d97daab9c01cfda19074e88cf85b
Kevin Hou <kevin.ch.hou@gmail.com>
2017-04-15 22:19:38 -0400

    Added basic resizing grid structure

--- data.js
+++ data.js
@@ -3,6 +3,35 @@ var data = [
     title: "Title",
     subtitle: "Subtitle",
     description: "Description",
+    image: "placeholder.png",
+    link: "#"
+  },
+  {
+    title: "Title 2",
+    subtitle: "Subtitle 2",
+    description: "Description 2",
+    image: "placeholder.png",
+    link: "#"
+  },
+  {
+    title: "Title 3",
+    subtitle: "Subtitle 3",
+    description: "Description 3",
+    image: "placeholder.png",
+    link: "#"
+  },
+  {
+    title: "Title 4",
+    subtitle: "Subtitle 4",
+    description: "Description 4",
+    image: "placeholder.png",
+    link: "#"
+  },
+  {
+    title: "Title 5",
+    subtitle: "Subtitle 5",
+    description: "Description 5",
+    image: "placeholder.png",
     link: "#"
   }
 ];
--- main.js
+++ main.js
@@ -1,18 +1,58 @@
+const gridID = 'grid'; // Grid id
+
 var GridView = React.createClass({
+
+  // Constructor
   getInitialState: function() {
     return {
-      data: data
+      data: data,
+      numColumns: 3, // Number of columns in grid
+      gridWidth: 0 // Will override immediately
     }
   },
+
+  // Establish sizing
+  componentDidMount: function() {
+    this.windowResize(); // Establish window size
+
+    // Add event listener for resizing
+    window.addEventListener('resize', this.windowResize);
+  },
+
+  // On window resize
+  windowResize: function() {
+    var gridWidth = $("#" + gridID).width(); // Get width of grid
+
+    this.setState({
+      gridWidth: gridWidth
+    });
+
+    // console.log("Set width to: " + gridWidth);
+  },
+
+  // Render the DOM
   render: function() {
+    // Establish standards
+    var baseWidth = this.state.gridWidth * Math.floor(100 / this.state.numColumns) / 100; // Get percentage width
+
     var tileNodes = this.state.data.map(function(tile) {
+      var width = baseWidth;
+      var height = width; // Square tile
+
+      // Size information to pass to child
+      var size = {
+        width: width,
+        height: height
+      }
+      console.log(tile);
       return (
-        <Tile title={tile.title} />
+        <Tile title = {tile.title}
+              image = {tile.image}
+              size = {size} />
       )
     })
     return (
       <div className="main-grid">
-        Hello
         {tileNodes}
       </div>
     );
@@ -21,8 +61,13 @@ var GridView = React.createClass({
 
 var Tile = React.createClass({
   render: function() {
+    var tileStyle = {
+      width: this.props.size.width,
+      height: this.props.size.height,
+      backgroundImage: 'url(' + this.props.image + ')'
+    };
     return (
-      <div className="tile">
+      <div className="tile" style={tileStyle}>
         {this.props.title}
       </div>
     )
@@ -31,5 +76,5 @@ var Tile = React.createClass({
 
 React.render(
   React.createElement(GridView, null),
-  document.getElementById('grid')
+  document.getElementById(gridID)
 );
